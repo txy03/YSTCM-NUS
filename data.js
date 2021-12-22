a23d767817aa80867b2dc5fe26fce1e50d7c98f8
txy03 <95894183+txy03@users.noreply.github.com>
2021-12-22 12:39:16 +0800

    Web tour of YSTCM at NUS
    A web tour that shows you what YSTCM is like.

---
+++ data.js
@@ -0,0 +1,152 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-alice-lee-plaza",
+      "name": "Alice Lee Plaza",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -2.4399290011405625,
+        "pitch": -0.21458774782765744,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.9863585396412127,
+          "pitch": 0.024769954299399544,
+          "rotation": 5.497787143782138,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4399290011405625,
+          "pitch": -0.21458774782765744,
+          "title": "Alice Lee Plaza",
+          "text": "<span style=\"font-family: Roboto, sans-serif; font-size: 15px;\">Inspirational life-affirming music-making is at the heart of the Yong Siew Toh Conservatory of Music (YST Conservatory) at the National University of Singapore. Centred in one of the world’s most dynamic countries, the Conservatory is uniquely placed to offer a distinct and powerful contemporary Asian voice.</span>"
+        }
+      ]
+    },
+    {
+      "id": "1-conservatory-foyer",
+      "name": "Conservatory Foyer",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.34737721169699043,
+        "pitch": 0.03742018664080149,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0284383128801284,
+          "pitch": 0.04724078055451386,
+          "rotation": 0.7853981633974483,
+          "target": "2-conservatory-lounge"
+        },
+        {
+          "yaw": -0.8341687417763843,
+          "pitch": 0.019767744941603382,
+          "rotation": 0.7853981633974483,
+          "target": "0-alice-lee-plaza"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.20249313275721903,
+          "pitch": 0.07653595274552316,
+          "title": "Conservatory Foyer&nbsp;",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, sans-serif; font-size: 15px; background-color: rgb(255, 255, 255);\">Located directly outside the Concert Hall and near the building entrance, the Foyer is an elegant space with high ceilings and a pleasing view of the Alice Lee Plaza, as well as the adjacent Lee Kong Chian Natural History Museum and the NUS Museum. The Foyer space is suitable for both informal (standing)&nbsp;and formal (sit-down) receptions.</span>"
+        }
+      ]
+    },
+    {
+      "id": "2-conservatory-lounge",
+      "name": "Conservatory Lounge",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.9101056842405946,
+        "pitch": -0.07876938576819725,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35180008985029687,
+          "pitch": 0.08148546847495375,
+          "rotation": 0,
+          "target": "1-conservatory-foyer"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.812444785058717,
+          "pitch": 0.283246195607056,
+          "title": "Conservatory Lounge&nbsp;",
+          "text": "<span style=\"color: rgb(0, 0, 0); font-family: Roboto, sans-serif; font-size: 15px; background-color: rgb(255, 255, 255);\">Nestled at the back of the Conservatory building between the Dean’s Office and the Administrative Office, the Conservatory Lounge is a comfortable space that invites visitors to sit down, relax and take in their surroundings. The Lounge is suitable for VIP cocktail receptions of not more than 100 persons. It also houses the A Tempo café, which sells a selection of food and beverages in the daytime on weekdays, and is open after hours during selected events.</span>"
+        }
+      ]
+    }
+  ],
+  "name": "Yong Siew Toh Conservatory of Music",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
